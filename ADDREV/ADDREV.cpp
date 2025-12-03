7b8fdc930b39ed308765bdc9f55e609e2b2d9213
JojoGid34 <lamidjan.jonathan@gmail.com>
2025-12-03 20:04:20 +0700

    Create ADDREV.cpp with initial setup
    Initialize ADDREV.cpp with basic structure and comments.

---
+++ ADDREV/ADDREV.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//Jadikan string, trs balik
+//Jadikan integer (bisa pakai longlong), add mereka
+//Jadikan string lagi, balik lagi, jadikan integer (biar 0 depan otomatis ilang)
+
+int main() {
+	
+	// your code here
+
+	return 0;
+}
